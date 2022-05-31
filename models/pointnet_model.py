88c5a489545b7d5e1d58be07756877b54eae1b20
raramanujan <raramanujan@davidson.edu@phy2kuchera.ada.davidson.edu>
2022-05-31 14:58:37 -0400

    Clean up + bug fixes

--- models/pointnet_model.py
+++ models/pointnet_model.py
@@ -51,26 +51,17 @@ def tnet(inputs, num_features):
     return layers.Dot(axes=(2, 1))([inputs, feat_T])
 
 def pnet(sem_seg_flag, num_points, num_classes):
-    #Since current number of classes is about 1/4 of pointnet, used 1/4 of size for all layers except initial 3 for input (x,y,z)    
+    # Since current number of classes is about 1/4 of pointnet, used 1/4 of size 
+    # for all layers except initial 3 for input (x,y,z)    
     inputs = keras.Input(shape=(num_points, 3))
     a = tnet(inputs, 3)
-    #print(a.shape)
-    b = conv_bn(a, 32) #
-    #print(b.shape)
-    c = conv_bn(b, 32) #
-    #print(c.shape)
-    d = tnet(c, 32) #
-    #print('x_transform: ' + str(d.shape))
-
-    e = conv_bn(d, 32) #
-    #print(e.shape)
-    f = conv_bn(e, 64) #
-    #print(f.shape)
+    b = conv_bn(a, 32)
+    c = conv_bn(b, 32)
+    d = tnet(c, 32)
+    e = conv_bn(d, 32) 
+    f = conv_bn(e, 64)
     g = conv_bn(f, 512)
-    #print(g.shape)
     x = layers.GlobalMaxPooling1D()(g)
-    #print(x.shape)
-
 
     if sem_seg_flag:
         #Current uses Fig 9 archituecture, can change to Fig 2 later
@@ -79,11 +70,7 @@ def pnet(sem_seg_flag, num_points, num_classes):
         long = layers.Concatenate(axis=2)([long,e])
         long = layers.Concatenate(axis=2)([long,f])
         x = tf.expand_dims(x, axis=1)
-        #print(x.shape)
         x = tf.repeat(x, repeats=512, axis=1)
-        #print(x.shape)
-#         x = layers.Concatenate(axis=2)([d,x])
-#         print(x.shape)
         long = layers.Concatenate(axis=2)([long,x])
         x = conv_bn(x, 256)
         x = conv_bn(long, 128)
@@ -92,25 +79,15 @@ def pnet(sem_seg_flag, num_points, num_classes):
         x = layers.Dropout(0.3)(x)
         x = conv_bn(x, 64)
         x = layers.Dropout(0.3)(x)
-        #outputs = conv_bn(x, num_classes)
         outputs = layers.Dense(num_classes, activation="softmax")(x)
-        model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
-        model.summary()
-
     else:
-        
         x = dense_bn(x, 256)
-        #print(tf.shape(x))
         x = layers.Dropout(0.3)(x)
-        #print(tf.shape(x))
         x = dense_bn(x, 128)
-        #print(tf.shape(x))
         x = layers.Dropout(0.3)(x)
-        #print(tf.shape(x))
         outputs = layers.Dense(num_classes, activation="softmax")(x)
-        #print(tf.shape(outputs))
-        exit()
-        model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
-        model.summary()
+
+    model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
+    model.summary()
         
     return model
