cc86cecf5a624c60f3409aff4bf8690b46316688
rramanujan <raramanujan@davidson.edu@phy2kuchera.ada.davidson.edu>
2022-05-31 21:14:14 -0400

    Various bug fixes -- successfully trains event-wise PointNet classification model.

--- event_classification.sh
+++ event_classification.sh
@@ -4,5 +4,6 @@
 #SBATCH --gpus 1
 #SBATCH --exclude=phy1kuchera
 
-python3 train_event_classification_model.py data/Mg22_size512train_convertXYZ.npy data/Mg22_size512val_convertXYZ.npy 
+python3 train_event_classification_model.py --num-classes 6 data/Mg22_size512_convertXYZ
+
 # python3 evaluate_pointnet.py $SLURM_JOB_ID $type $sem_seg $num_classes $voxel_shuffle
--- train_event_classification_model.py
+++ train_event_classification_model.py
@@ -6,12 +6,7 @@ from plotting import plot_learning_curve
 from pointnet_model import pnet
 from datetime import datetime
 import click
-
-
-def fix_shape(points, labels):
-    # points already have the correct shape
-    labels = tf.reshape(labels, (512, 1))
-    return points, labels
+import os
 
 
 @click.command()
@@ -19,34 +14,38 @@ def fix_shape(points, labels):
 @click.option('--batch-size', default=32, type=click.INT, help='Batch size for SGD')
 @click.option('--num-classes', default=2, type=click.INT, help='Number of classes to predict')
 @click.option('--num-epochs', default=10, type=click.INT, help='Number of epochs of training')
-@click.argument('train-file-stem')
-@click.argument('val-file-stem')
-def train(num_points, batch_size, num_classes, num_epochs, train_file_stem, val_file_stem):
-    train_points = np.load(train_file_stem + '_points.npy')
-    train_labels = np.load(train_file_stem + '_labels.npy')
+@click.argument('file-stem')
+def train(num_points, batch_size, num_classes, num_epochs, file_stem):
 
-    val_points = np.load(val_file_stem + '_points.npy')
-    val_labels = np.load(val_file_stem + '_labels.npy')
+    # load training data
+    train_features_filename = file_stem + '_train_features.npy'
+    train_labels_filename = file_stem + '_train_labels.npy'
+    train_features = np.load(train_features_filename)
+    train_labels = np.load(train_labels_filename)
+    train_ds = tf.data.Dataset.from_tensor_slices((train_features, train_labels))
+    train_ds = train_ds.batch(batch_size, drop_remainder=True)
 
-    # need to drop remainder?
-    train_ds = tf.data.Dataset.from_tensor_slices((train_points, train_labels))
-    val_ds = tf.data.Dataset.from_tensor_slices((val_points, val_labels))
-    
-    train_ds = train_ds.shuffle(len(train_ds)).batch(batch_size)
-    val_ds = val_ds.shuffle(len(val_ds)).batch(batch_size)
-    
+    # load validation data
+    val_features_filename = file_stem + '_val_features.npy'
+    val_labels_filename = file_stem + '_val_labels.npy'
+    val_features = np.load(val_features_filename)
+    val_labels = np.load(val_labels_filename)
+    val_ds = tf.data.Dataset.from_tensor_slices((val_features, val_labels))
+    val_ds = val_ds.batch(batch_size, drop_remainder=True)
+
+    # build and train model
     model = pnet(sem_seg_flag=False, num_points=num_points, num_classes=num_classes)
-    
     model.compile(loss="sparse_categorical_crossentropy",
                   optimizer=keras.optimizers.Adam(learning_rate=0.0005),
                   metrics=["sparse_categorical_accuracy"])
+    history = model.fit(train_ds, validation_data=val_ds, epochs=num_epochs, verbose=1)
     
-    history = model.fit(train_ds, validation_data=val_ds, epochs=num_epochs, verbose = 1)
-    
+    # save model and plot learning curve
     timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
     model_file_path = 'models/{}/weights'.format(timestamp)
     model.save_weights(filepath=model_file_path)
     
+    os.makedirs('plots/{}'.format(timestamp))
     plot_file_path = 'plots/{}/learning_curve.png'.format(timestamp)
     plot_learning_curve(history, plot_file_path)
     
