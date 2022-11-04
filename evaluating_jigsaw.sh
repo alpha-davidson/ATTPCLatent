dcee4c874846f75da4b5b9133af9322007cecbdf
dvsharma <dvsharma@davidson.edu@phy2kuchera.ada.davidson.edu>
2022-11-04 10:32:03 -0400

    Updated plotting file to include random event plots, and in turn updated evaluation file to visualize random events

--- evaluating_jigsaw.sh
+++ evaluating_jigsaw.sh
@@ -4,6 +4,6 @@
 #SBATCH --gpus 1
 
 # Adapt the models sub-folder as needed to the correct time stamp
-python3 evaluate_jigsaw_reconstruction.py --num-classes 27 models/2022-10-21-13:12:50/weights \
+python3 evaluate_jigsaw_reconstruction.py --num-classes 27 models/2022-11-03-10:26:40/weights \
           voxel_data/Mg22_size512
           
