dee1f74c1dc9a43995a66ab96750299d2c9cab7f
dvsharma <dvsharma@davidson.edu@phy2kuchera.ada.davidson.edu>
2022-11-10 22:39:01 -0500

    Modified files for training w/ 20 epochs

--- unscrambling_jigsaw.sh
+++ unscrambling_jigsaw.sh
@@ -3,4 +3,4 @@
 #SBATCH --mem 32G
 #SBATCH --gpus 1
 
-python3 pretrain_on_jigsaw_events.py --num-classes 27 --num-epochs 50 voxel_data/Mg22_size512
+python3 pretrain_on_jigsaw_events.py --num-classes 27 --num-epochs 20 voxel_data/Mg22_size512
