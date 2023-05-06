3ff36207d2ca7f34d36d8c4aa43cdabddee43864
prito <prito@davidson.edu@phy4kuchera.ada.davidson.edu>
2023-05-06 16:05:11 -0400

    added docs

--- evaluating_jigsaw.sh
+++ evaluating_jigsaw.sh
@@ -3,7 +3,8 @@
 #SBATCH --mem 32G
 #SBATCH --gpus 1
 
-# Adapt the models sub-folder as needed to the correct time stamp
-python3 evaluate_jigsaw_reconstruction.py --num-classes 27 models/2023-01-05-03:15:31/weights \
+# Adapt the models sub-folder as needed to the correct file name. Also, to plot the histogram, make sure the name
+# of the sub-folder matches up between the models and plots folder
+python3 evaluate_jigsaw_reconstruction.py --num-classes 27 models/70epochs-notbestmodel-4000events/weights \
           voxel_data/Mg22_size512
           
