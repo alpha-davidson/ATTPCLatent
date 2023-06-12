72fd0d895b3e6f974e4553700b002573f2694877
yuahmed1 <yuahmed1@davidson.edu@phy3kuchera.ada.davidson.edu>
2023-06-12 16:54:26 -0400

    modified to work with checkpoint weights, plot overlay.

--- evaluate_jigsaw_reconstruction.py
+++ evaluate_jigsaw_reconstruction.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
 from pointnet_model import pnet
 from plotting import plot_events, plot_histogram, plot_overlay
-
+import os
 
 @click.command()
 @click.option('--num-points', default=512, type=click.INT, help='Number of points per event')
@@ -36,22 +36,20 @@ def evaluate(num_points, num_classes, model_file_stem, data_file_stem):
     model_name = model_file_stem.split("/")[-1] #modified to save model/ckpt name
     model_time =  model_file_stem.split("/")[-3] #added to save timestamp
     print('Mean accuracy: {}'.format(np.mean(test_labels == predictions))) #point-wise accuracy
-#    plot_events(test_labels, predictions, data_file_stem, model_name, model_time)
+    #plot_events(test_labels, predictions, data_file_stem, model_name, model_time)
     
     # create histogram of percent accuracy
-    
     percent_accuracy = np.mean(test_labels == predictions, axis=1) #along X axis
-#    plot_histogram(model_name, model_time, percent_accuracy)   
-    
-    #create overlay of two histograms already constructed
-    
-    #TODO: ensure histograms being passed exist
+    #plot_histogram(model_name, model_time, percent_accuracy)   
+   
+    #create overlay of two histograms
+        #modify path1 and path2 as needed.
     path1 = "plots/{}/cp-035.ckpt/percent_accuracy_histogram.png".format(model_time)
-    path2 = "plots/2023-06-06-12:58:21/percent_accuracy_histogram.png".format(model_time)
-    plot_overlay(path1, path2)
-
-    #YFA:
-    #before i forget again: 
-    #compares predictions of the third column (index 2) against what it actually is, calculates means of that. 
+    path2 = "plots/{}/cp-032.ckpt/percent_accuracy_histogram.png".format(model_time)
+    #check histograms exist 
+    if os.path.exists(path1) and os.path.exists(path2): 
+        plot_overlay(path1, path2)
+    
+    #TODO: observe trends within events in 0 and 1 bins of histogram.
 if __name__ == '__main__':
     evaluate()
