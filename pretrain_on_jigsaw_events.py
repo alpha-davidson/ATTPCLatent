65cf1c31bf51796e014f6630bdebd0b92ae500f2
yuahmed1 <yuahmed1@davidson.edu@phy3kuchera.ada.davidson.edu>
2023-06-12 15:31:22 -0400

    updated to save weights for multiple models.

--- pretrain_on_jigsaw_events.py
+++ pretrain_on_jigsaw_events.py
@@ -45,15 +45,23 @@ def train(num_points, batch_size, num_classes, num_epochs, file_stem):
     
     # save model and plot learning curve
     timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-    model_file_path = 'models/{}/weights'.format(timestamp)
+    #model_file_path = 'models/weights/{}'.format(timestamp)
     
+    checkpoint_path = "models/{}/weights/cp".format(timestamp)
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    checkpoint_path = checkpoint_path + "-{epoch:03d}.ckpt"
+        
     # callback checkpoint to save best model
+    # modified to save model every [save_freq] epochs
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=model_file_path,
+        filepath=checkpoint_path, #model_file_path,
         save_weights_only = True,
         monitor = 'val_accuracy',
         mode = 'max',
-        save_best_only = True)
+        save_best_only = False,
+        save_freq = (7*batch_size)) #determines frequency of saving model
+    
+    model.save_weights(checkpoint_path.format(epoch=0))
     
     #reduce LR checkpoint to adjust LR upon no improvement
     reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
@@ -61,16 +69,16 @@ def train(num_points, batch_size, num_classes, num_epochs, file_stem):
         factor=0.5,
         patience=5,
         mode='auto',
+        verbose=1,
         min_delta=0.001,
         min_lr=0.00001)
-        # verbose=0,
         # cooldown=0,
         # min_lr=0
     
     # build model and fit model
     model.summary()
     model.compile(loss="sparse_categorical_crossentropy",
-                  optimizer=keras.optimizers.Adam(learning_rate=0.005), #prev LR: 0.0005
+                  optimizer=keras.optimizers.Adam(learning_rate=0.005), #prev LR: 0.0005, performs better with new. 
                   metrics=["sparse_categorical_accuracy", "accuracy"])
     history = model.fit(train_ds, validation_data=val_ds, epochs=num_epochs, 
                         callbacks=[checkpoint_callback, reduce_lr], verbose=1)
