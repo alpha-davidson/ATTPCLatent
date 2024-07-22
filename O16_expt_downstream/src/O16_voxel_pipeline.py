6efa445a1d2796281c4d634096184a99de7145dc
alfinch <alfinch@davidson.edu@alpha0.ada.davidson.edu>
2024-07-22 17:01:56 -0400

    save

--- O16_expt_downstream/src/O16_voxel_pipeline.py
+++ O16_expt_downstream/src/O16_voxel_pipeline.py
@@ -27,8 +27,66 @@ import os
 
 # user defined functions...
 
-def filter_data():
+def convert_data(data):
     
+    keys = list(data.keys())
+    
+    # making array of event lengths
+    event_lens = np.zeros(len(keys), int)
+    for i in range(len(keys)):
+        event = keys[i]
+        event_lens[i] = len(data[event])
+    np.save('../voxel_data/O16_event_lens.npy', event_lens)
+    
+    # making a numpy array of data
+    event_data = np.zeros((len(keys), np.max(event_lens), 6), float)
+    for n in tqdm.tqdm(range(len(keys))):
+        name = keys[n]
+        event = data[name]
+        ev_len = len(event)
+        for i,e in enumerate(event):
+            instant = np.array(list(e))
+            event_data[n,i,:5] = instant[:5]
+            event_data[n,i,5] = float(n) # storing the event index
+    event_data[56437,0,5] = 56437 # fixing the empty event ('Event 60795,' index 56437)
+    np.save('../voxel_data/O16_w_event_keys.npy', event_data)
+    
+def _filter_point_clouds(data):
+    
+    # Extract only the x, y, z coordinates, and the charge (columns 0, 1, 2, 4)
+    filtered_data = data[..., [0, 1, 2, 4]]
+    
+    return filtered_data
+
+def filter_data(sample_size, TRACK_CLASS, ISOTOPE):
+    
+    data = np.load('../voxel_data/' + ISOTOPE + '_w_event_keys.npy')
+    event_lens = np.load('../voxel_data/' + ISOTOPE + '_event_lens.npy')
+    data[56437,0,-1] = 56437
+    
+    # Apply the filter function to the data
+    filtered_data = _filter_point_clouds(data)
+    
+    # Apply additional filtering based on the charge value
+    filtered_data[:,:,0] = np.where(filtered_data[:,:,3] < 90, 0, filtered_data[:,:,0])
+    filtered_data[:,:,1] = np.where(filtered_data[:,:,3] < 90, 0, filtered_data[:,:,1])
+    filtered_data[:,:,2] = np.where(filtered_data[:,:,3] < 90, 0, filtered_data[:,:,2])
+    filtered_data[:,:,3] = np.where(filtered_data[:,:,3] < 90, 0, filtered_data[:,:,3])
+    
+    # Determine threshold for minimum number of non-zero points
+    min_points_threshold = 70
+    
+    # Filter out events with too few non-zero points
+    filtered_events = []
+    valid_event_indices = []
+    for i in range(filtered_data.shape[0]):
+        if np.count_nonzero(filtered_data[i, :, 0]) >= min_points_threshold:
+            filtered_events.append(filtered_data[i])
+            valid_event_indices.append(i)
+    
+    filtered_data = np.array(filtered_events)
+    
+    np.save('../voxel_data/O16_filtered_data.npy', filtered_data)
 
 def main():
     
@@ -42,7 +100,8 @@ def main():
     data = h5py.File('../voxel_data/O16_run160.h5','r')
 
     # calling the functions
-
+    '''convert_data(data)'''
+    filter_data(sample_size, TRACK_CLASS, ISOTOPE)
 
 if __name__ == "__main__":
     
