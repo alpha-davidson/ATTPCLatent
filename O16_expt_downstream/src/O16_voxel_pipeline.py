448a55df3a2dc009e027f84afbf99e5c883086d6
alfinch <alfinch@davidson.edu@alpha0.ada.davidson.edu>
2024-07-23 11:06:28 -0400

    routine save

--- O16_expt_downstream/src/O16_voxel_pipeline.py
+++ O16_expt_downstream/src/O16_voxel_pipeline.py
@@ -58,7 +58,7 @@ def _filter_point_clouds(data):
     
     return filtered_data
 
-def filter_data(sample_size, TRACK_CLASS, ISOTOPE):
+def filter_data(ISOTOPE, min_points_threshold):
     
     data = np.load('../voxel_data/' + ISOTOPE + '_w_event_keys.npy')
     event_lens = np.load('../voxel_data/' + ISOTOPE + '_event_lens.npy')
@@ -72,10 +72,7 @@ def filter_data(sample_size, TRACK_CLASS, ISOTOPE):
     filtered_data[:,:,1] = np.where(filtered_data[:,:,3] < 90, 0, filtered_data[:,:,1])
     filtered_data[:,:,2] = np.where(filtered_data[:,:,3] < 90, 0, filtered_data[:,:,2])
     filtered_data[:,:,3] = np.where(filtered_data[:,:,3] < 90, 0, filtered_data[:,:,3])
-    
-    # Determine threshold for minimum number of non-zero points
-    min_points_threshold = 70
-    
+        
     # Filter out events with too few non-zero points
     filtered_events = []
     valid_event_indices = []
@@ -85,8 +82,128 @@ def filter_data(sample_size, TRACK_CLASS, ISOTOPE):
             valid_event_indices.append(i)
     
     filtered_data = np.array(filtered_events)
+    valid_event_indices = np.array(valid_event_indices)
     
-    np.save('../voxel_data/O16_filtered_data.npy', filtered_data)
+    np.save('../voxel_data/O16_filtered_data', filtered_data)
+    np.save('../voxel_data/O16_valid_event_indices', valid_event_indices)
+
+def random_sample(ISOTOPE, sample_size, dimension):
+
+    data = np.load('../voxel_data/' + ISOTOPE + '_w_event_keys.npy')
+    filtered_data = np.load('../voxel_data/O16_filtered_data.npy')
+    valid_event_indices = np.load('../voxel_data/O16_valid_event_indices.npy')
+    
+    new_array_name = ISOTOPE + '_size' + str(sample_size) + '_sampled'
+    new_data = np.zeros((filtered_data.shape[0], sample_size, 6), float)
+    # Using tqdm with enumerate for progress indication
+    for idx, original_idx in tqdm.tqdm(enumerate(valid_event_indices), total=len(valid_event_indices)):
+        # Filter out zero values using idx instead of original_idx
+        non_zero_points = filtered_data[idx][filtered_data[idx, :, 0] != 0]
+        non_zero_len = non_zero_points.shape[0]
+    
+        if original_idx == 56437:  # skipping the one empty event
+            new_data[idx, 0, 5] = 56437
+            continue
+    
+        if non_zero_len > sample_size:
+            random_points = np.random.choice(non_zero_len, sample_size, replace=False)  # choosing the random instances to sample
+            for count, r in enumerate(random_points):
+                new_data[idx, count, :3] = non_zero_points[r, :3]  # Only use the filtered x, y, z
+                new_data[idx, count, 3] = data[original_idx, r, 3]  # adding time from original data
+                new_data[idx, count, 4] = non_zero_points[r, 3]  # use amplitude (charge) from filtered data
+                new_data[idx, count, 5:] = data[original_idx, r, 5:]  # Add the remaining columns (event index) from the original data
+        else:
+            new_data[idx, :non_zero_len, :3] = non_zero_points[:, :3]  # Only use the filtered x, y, z
+            new_data[idx, :non_zero_len, 3] = data[original_idx, :non_zero_len, 3]  # adding time from original data
+            new_data[idx, :non_zero_len, 4] = non_zero_points[:, 3]  # use amplitude (charge) from filtered data
+            new_data[idx, :non_zero_len, 5:] = data[original_idx, :non_zero_len, 5:]  # Add the remaining columns (event index) from the original data
+            need = sample_size - non_zero_len
+            random_points = np.random.choice(non_zero_len, need, replace=True if need > non_zero_len else False)
+            for count, r in enumerate(random_points, start=non_zero_len):
+                new_data[idx, count, :3] = non_zero_points[r, :3]  # Only use the filtered x, y, z
+                new_data[idx, count, 3] = data[original_idx, r, 3]  # adding time from original data
+                new_data[idx, count, 4] = non_zero_points[r, 3]  # use amplitude (charge) from filtered data
+                new_data[idx, count, 5:] = data[original_idx, r, 5:]  # Add the remaining columns (event index) from the original data
+        new_data[idx, 0, 5] = data[original_idx, 0, 5]  # saving the event index
+    
+    # Verify if there are still any zero values in new_data
+    print("Final check of new_data for zeros")
+    print("Number of zero values in x, y, z, charge columns:", np.count_nonzero(new_data[:, :, :4] == 0))
+    print("Indices of zero values:", np.where(new_data[:, :, :4] == 0))
+    
+    # If there are still zeros, print a few samples where zeros are present
+    zero_indices = np.where(new_data[:, :, :4] == 0)
+    if len(zero_indices[0]) > 0:
+        for i in range(min(5, len(zero_indices[0]))):
+            print(f"Zero found at index {zero_indices[0][i]}, event {zero_indices[1][i]}, column {zero_indices[2][i]}")
+    
+    assert np.all(new_data[:, :, :4] != 0), 'new_data contains zero values in the x, y, z, or charge columns'
+    
+    np.save('../voxel_data/' + new_array_name, new_data)
+    
+    assert new_data.shape == (filtered_data.shape[0], sample_size, 6), 'Array has incorrect shape'
+    assert len(np.unique(new_data[:, :, 5])) == filtered_data.shape[0], 'Array has incorrect number of events'
+
+    size = len(new_data)
+    dataset = np.zeros((size, sample_size, dimension + 3), float)
+    count = 0
+
+    for i in range(size):
+        dataset[count,:,:3] = new_data[count,:,:3] # x, y, z
+        if dimension == 4: 
+            dataset[count,:,3] = new_data[count,:,4] # charge
+        dataset[count,0,-3] = new_data[count,0,5] # event index
+        # [count,0,-2] -- number of tracks, leave as 0
+        dataset[count,0,-1] = np.count_nonzero(new_data[count,:,0])# length of event 
+        count += 1
+    
+    np.save('../voxel_data/' + ISOTOPE + '_size' + str(sample_size), dataset)
+
+def scale_and_split(ISOTOPE, sample_size):
+
+    dataset = np.load('../voxel_data/' + ISOTOPE + '_size' + str(sample_size) + '.npy')
+
+    # scale
+
+    # values correspond to the x,y,z,charge index
+    values = [0,1,2,3] 
+    means_and_stds = []
+    min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1))
+    dataset[:,:,3] = np.log(dataset[:,:,3]) # log scale charge
+    # standard scaling 
+    for n in values:
+        mean = np.mean(dataset[:,:,n])
+        std = np.std(dataset[:,:,n])
+        means_and_stds.append([mean,std])
+        dataset[:,:,n] = (dataset[:,:,n] - mean) / std
+    dataset[:,0,-1] = np.log(dataset[:,0,-1])
+    dataset[:,0,-1] = min_max_scaler.fit_transform(dataset[:,0,-1].reshape(-1, 1)).reshape(1,-1)
+    
+    assert np.sum(np.isnan(dataset)) == 0, 'NaNs in dataset'
+    assert np.sum(np.isinf(dataset)) == 0, 'Infinities in dataset'
+
+    # split
+    
+    rand_shuffle = np.random.choice(len(dataset), len(dataset), replace = False)
+    name = ISOTOPE + '_size' + str(sample_size)
+    
+    # 20-20 marking for test and validation
+    test_split = int(len(dataset) * .2)
+    val_split = int(len(dataset) * .4)
+    
+    test = dataset[rand_shuffle[:test_split],:,:]
+    val = dataset[rand_shuffle[test_split:val_split],:,:]
+    train = dataset[rand_shuffle[val_split:],:,:]
+    print(len(dataset))
+    print(test.shape, val.shape, train.shape)
+    
+    os.makedirs('../data_splits/')
+    np.save('../data_splits/' + ISOTOPE + '_size' + str(sample_size)+'_test', test)
+    np.save('../data_splits/' + ISOTOPE + '_size' + str(sample_size)+'_val', val)
+    np.save('../data_splits/' + ISOTOPE + '_size' + str(sample_size)+'_train', train)
+    assert len(np.unique(np.isnan(train[:,:,4]))) == 1, 'NaNs in dataset'
+    assert len(np.unique(np.isnan(val[:,:,4]))) == 1, 'NaNs in dataset'
+    assert len(np.unique(np.isnan(test[:,:,4]))) == 1, 'NaNs in dataset'
 
 def main():
     
@@ -95,13 +212,16 @@ def main():
     TRACK_CLASS = False
     dimension = 4 # desired dimension of data to be input
     ISOTOPE = 'O16'
-
+    min_points_threshold = 70 # Determine threshold for minimum number of non-zero points (to be used in the filter_data function)
+    
     # could try to change to other isotope data in the future
     data = h5py.File('../voxel_data/O16_run160.h5','r')
 
     # calling the functions
-    '''convert_data(data)'''
-    filter_data(sample_size, TRACK_CLASS, ISOTOPE)
+    '''convert_data(data)
+    filter_data(ISOTOPE, min_points_threshold)
+    random_sample(ISOTOPE, sample_size, dimension)'''
+    scale_and_split(ISOTOPE, sample_size)
 
 if __name__ == "__main__":
     
