5ba8dadebcd532b1d9c3f2673f22546832ecdeb9
Ann Zhu <anzhu@davidson.edu>
2024-09-05 11:45:07 -0400

    C16 downstream pipeline

--- C16_evaluating_jigsaw.sh
+++ C16_evaluating_jigsaw.sh
@@ -5,4 +5,4 @@
 
 # Adapt the models sub-folder as needed to the correct file name. Also, to plot the histogram, make sure the name
 # of the sub-folder matches up between the models and plots folder
-python3 C16_evaluate_jigsaw_reconstruction.py --num-classes 24 C16_models/2024-07-23-13:28:34/weights/cp-099.ckpt C16_downstream/data/C16_size512
+python3 C16_evaluate_jigsaw_reconstruction.py --num-classes 24 C16_models/2024-07-25-10:32:38/weights/cp-055.ckpt C16_downstream/data/C16_size512
