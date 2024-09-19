7c7bed7ce35bc4920f2523bb4596ddcc60c560e9
Ann Zhu <anzhu@davidson.edu>
2024-09-18 21:14:09 -0400

    rename C16 folder

--- C16_unscrambling_jigsaw.sh
+++ C16_unscrambling_jigsaw.sh
@@ -3,4 +3,4 @@
 #SBATCH --mem 32G
 #SBATCH --gpus 1
 
-python3 C16_pretrain_on_jigsaw_events.py --num-classes 24 --num-epochs 100 C16_downstream/data/C16_size512
+python3 C16_pretrain_on_jigsaw_events.py --num-classes 24 --num-epochs 100 C16_pretrain/data/C16_size512
