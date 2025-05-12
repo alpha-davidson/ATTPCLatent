4aebb15a07fc98df3de4d990c0766af7eedd294a
Ann Zhu <anzhu@davidson.edu>
2025-05-12 10:51:26 -0400

    delete redundent data pipelines for O16 and C16

--- pretrain/data_processing/O16/pipeline/O16_convert_data_runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name "O16_Convert_The_Data"
-#SBATCH --mem 32G
-#SBATCH --gpus 1
-
-# Calling this in the terminal will run the file 'O16_convert_data.py'
-python3 O16_convert_data.py O16_expt_downstream/src O16_expt_downstream/voxel_data
