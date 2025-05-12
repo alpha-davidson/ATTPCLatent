e8656da864c5196853b639e8ce322eccdb299757
Ann Zhu <anzhu@davidson.edu>
2025-05-12 11:15:52 -0400

    remove downstream_tasks folder

--- downstream_tasks/Mg22_track_counting/training/pretrain_model.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name "Mg22_DOWNSTREAM"
-#SBATCH --mem 32G
-#SBATCH --gpus 1
-
-python3 pretrain_model.py --model-num 2174
--- downstream_tasks/O16_track_counting/training/pretrain_model.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name "O16_DOWNSTREAM"
-#SBATCH --mem 32G
-#SBATCH --gpus 1
-
-python3 pretrain_model.py
