6a0ea1cbb1f88d4c572e5fbb64eacc632bdfc6fd
nialeksii <nialeksii@davidson.edu@alpha2.ada.davidson.edu>
2025-06-12 13:45:52 -0400

    changed the batch size to 64 for better accuracy

--- training/unscrambling_jigsaw.sh
+++ training/unscrambling_jigsaw.sh
@@ -3,4 +3,4 @@
 #SBATCH --mem 32G
 #SBATCH --gpus 1
 
-python pretrain_on_jigsaw_events.py --beam O16 --num-classes 24 --num-epochs 200 ../data_processing/O16/voxel_data/O16_size512
+python pretrain_on_jigsaw_events.py --beam O16 --batch-size 64 --num-classes 24 --num-epochs 200 ../data_processing/O16/voxel_data/O16_size512
