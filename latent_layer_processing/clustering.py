5f527f98f97da96dd3b0f07d3a9fc74470b63c12
nialeksii <nialeksii@davidson.edu@alpha2.ada.davidson.edu>
2025-06-26 14:45:45 -0400

    added a functionality for saving/reproducing results of clustering and plotting random events of a particular region in a cluster

--- latent_layer_processing/clustering.py
+++ latent_layer_processing/clustering.py
@@ -5,19 +5,20 @@ from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+import umap
 
 
-def t_SNE_clustering(features, labels, data_file_stem, dimension, ax, color, label, alpha):
+def t_SNE_clustering(features, dimension, ax, color, label, alpha, perplexity):
     # initialize properties for t-SNE clustering
-    PERPLEXITY = 40
+    PERPLEXITY = perplexity
     CLUSTER_DIMENSIONALITY = dimension
 
     # create a folder for t-SNE clustering
-    folder_path = f'./clustering_plots/t_sne'
+    folder_path = f'./plots/t_sne'
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     # create a folder for plots of particular dimensionality
-    folder_path = f'./clustering_plots/t_sne/{CLUSTER_DIMENSIONALITY}d_plots'
+    folder_path = f'./plots/t_sne/{CLUSTER_DIMENSIONALITY}d_plots'
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
@@ -45,19 +46,57 @@ def t_SNE_clustering(features, labels, data_file_stem, dimension, ax, color, lab
     
         # visualize the data on a 3D plot        
         ax.scatter(x, y, z, color=color, label=label, s=5, alpha=alpha)
+    return tsne_data 
 
+def UMAP_embedding(features, dimension, ax, color, label, alpha, neighbors):
+    # initialize properties for UMAP embedding
+    NEIGHBORS = neighbors
+    CLUSTER_DIMENSIONALITY = dimension
 
-def k_means_clustering(features, labels, data_file_stem, dimension):    
+    # create a folder for UMAP embedding
+    folder_path = f'./plots/umap'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    # create a folder for plots of particular dimensionality
+    folder_path = f'./plots/umap/{CLUSTER_DIMENSIONALITY}d_plots'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    # apply UMAP embedding
+    model = umap.UMAP(n_components=CLUSTER_DIMENSIONALITY, n_neighbors=NEIGHBORS)
+    umap_data = model.fit_transform(features)
+    print(umap_data)
+    # plot the clusters in 2D
+    if (dimension == 2):
+        # extract x- and y-axis values
+        x = np.array(umap_data[:, 0])
+        y = np.array(umap_data[:, 1])
+    
+        # # visualize the data on a 2D scatter plot
+        ax.scatter(x, y, color=color, label=label, s=5, alpha=alpha)
+
+    # plot the clusters in 3D
+    if (dimension == 3):
+        # extract x-, y-, and z-axis values
+        x = np.array(umap_data[:, 0])
+        y = np.array(umap_data[:, 1])
+        z = np.array(umap_data[:, 2])
+    
+        # visualize the data on a 3D plot        
+        ax.scatter(x, y, z, color=color, label=label, s=5, alpha=alpha)
+    return umap_data
+    
+def k_means_clustering(features, labels, dimension):    
     # initialize properties for k-means clustering
     CLUSTER_DIMENSIONALITY = dimension
     k = 4
 
     # create a folder for k-means clustering
-    folder_path = f'./clustering_plots/k_means'
+    folder_path = f'./plots/k_means'
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     # create a folder for plots of particular dimensionality
-    folder_path = f'./clustering_plots/k_means/{CLUSTER_DIMENSIONALITY}d_plots'
+    folder_path = f'./plots/k_means/{CLUSTER_DIMENSIONALITY}d_plots'
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     
@@ -89,7 +128,7 @@ def k_means_clustering(features, labels, data_file_stem, dimension):
                     marker='x', s=5, linewidths=3,
                     color='k', zorder=10)
         plt.legend()
-        plt.savefig(f'clustering_plots/k_means/2d_plots/plot_k_{k}.png')
+        plt.savefig(f'plots/k_means/2d_plots/plot_k_{k}.png')
 
     # plot the clusters in 3D
     if (dimension == 3):
@@ -108,6 +147,7 @@ def k_means_clustering(features, labels, data_file_stem, dimension):
                    marker='x', s=5, linewidths=3,
                    color='k', zorder=10, label='Centroids')
         ax.legend()
-        plt.savefig(f'clustering_plots/k_means/3d_plots/plot_k_{k}.png')
+        plt.savefig(f'plots/k_means/3d_plots/plot_k_{k}.png')
     
     plt.close()
+    return kmeans_data
--- latent_layer_processing/global_features_extraction.py
+++ latent_layer_processing/global_features_extraction.py
@@ -2,24 +2,54 @@ import os
 import sys
 import click
 import numpy as np
+import random
 import tensorflow as tf
 from tensorflow import keras
 from ..pointnet import create_pointnet_model
+from ..simulated_data.plot_data import plot_event
 from clustering import t_SNE_clustering
+from clustering import UMAP_embedding
 from clustering import k_means_clustering
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from mpl_toolkits import mplot3d
+from matplotlib import colors
+import matplotlib.cm as cm
+from sklearn.preprocessing import StandardScaler
 
 def labels_extraction(data_file_stem, BATCH_SIZE):
-    test_ds = np.load('{}{}'.format(data_file_stem, 'test.npy'))
-    test_features = tf.data.Dataset.from_tensor_slices(test_ds[:, :, :4]).batch(BATCH_SIZE)
-    test_labels = test_ds[:, :, 4]
-    return test_features, test_labels
+    ds = np.load('{}{}'.format(data_file_stem, '.npy'))
+    features = tf.data.Dataset.from_tensor_slices(ds[:, :, :4]).batch(BATCH_SIZE)
+    labels = ds[:, :, 4]
+    return features, labels
 
 def global_features(model, data_file_stem, BATCH_SIZE):
-    test_features, test_labels = labels_extraction(data_file_stem, BATCH_SIZE)
-    global_features = model.predict(test_features)
-    return global_features, test_labels
+    features, labels = labels_extraction(data_file_stem, BATCH_SIZE)
+    global_features = model.predict(features)
+    return global_features, labels
 
+def plot_umap_events(umap_data, name, data_file_stem):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    sampled_data = np.load(f'{data_file_stem}.npy')
+
+    folder_path = f'./plots/umap/2d_plots/events_{name}'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    j = 0
+    i = 0
+    maxim = 0
+    while j != 10:   
+        if (maxim == len(umap_data)):
+            break
+        if (umap_data[i][0] >= 2 and umap_data[i][0] < 7 and umap_data[i][1] >= 2.5 and umap_data[i][1] < 6): # change the boundaries
+            fig = plt.figure(figsize=(10, 10))
+            plot_event(fig, sampled_data, i)
+            plt.savefig(f'./plots/umap/2d_plots/events_{name}/event_{i}.png')
+            j+=1
+        i = random.randint(0, len(umap_data) - 1)
+        maxim+=1
+            
 @click.command()
 @click.option('--beam', default='O16', type=click.STRING, help='The beam to train on (e.g. O16, Mg22, C16)')
 @click.option('--num-points', default=512, type=click.INT, help='Number of points per event')
@@ -34,41 +64,52 @@ PYTHONPATH=../.. python3 -m ATTPCLatent.latent_layer_processing.global_features_
     """
     # load model
     model = tf.keras.models.load_model(model_folder)    
-    
     BATCH_SIZE = 32
 
     # extract global features from latent space
     global_feature_extractor = keras.Model(inputs=model.input, outputs=model.get_layer("latent_space").output)
     
-    # create a folder for results of clustering
-    folder_path = "./clustering_plots"
+    # load global features (change import files or comment this section if needed)
+    global_features_2track, test_labels_2track = global_features(global_feature_extractor, '../simulated_data/process_data/O16/voxel_data/output_digi_HDF_2Body/O16_size512', BATCH_SIZE)
+    global_features_3track, test_labels_3track = global_features(global_feature_extractor, '../simulated_data/process_data/O16/voxel_data/output_digi_HDF_3Body/O16_size512', BATCH_SIZE)
+    global_features_Mg22, test_labels__Mg22 = global_features(global_feature_extractor, '../simulated_data/process_data/Mg22/voxel_data/output_digi_HDF_Mg22_Ne20pp_8MeV/Mg22_size512', BATCH_SIZE)
+
+    
+    # create a folder for results of clustering/embedding
+    folder_path = "./plots"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
-    # load test data
-    global_features_original, test_labels_original = global_features(global_feature_extractor, data_file_stem, BATCH_SIZE)
-    global_features_2Body, test_labels_2Body = global_features(global_feature_extractor, '../simulated_data/voxel_data/output_digi_HDF_2Body/O16_size512', BATCH_SIZE)
-    global_features_2Body_2T, test_labels_2Body_2T = global_features(global_feature_extractor, '../simulated_data/voxel_data/output_digi_HDF_2Body_2T/O16_size512', BATCH_SIZE)
-
-    # plot t-SNE clustering of experimental, 2Body, and 2Body_2T global features in 2D space
-    fig = plt.figure()
+    
+    #plot umap embeddings (or comment if needed)
+    fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(111)
-    t_SNE_clustering(global_features_2Body, test_labels_2Body, '../simulated_data/voxel_data/output_digi_HDF_2Body/O16_size512', 2, ax, 'red', 'output_digi_HDF_2Body', 1)
-    t_SNE_clustering(global_features_2Body_2T, test_labels_2Body_2T, '../simulated_data/voxel_data/output_digi_HDF_2Body_2T/O16_size512', 2, ax, 'green', 'output_digi_HDF_2Body_2T', 1)
-    t_SNE_clustering(global_features_original, test_labels_original, data_file_stem, 2, ax, 'blue', 'experimental', 0.3)
+    neighbors = 250
+    umap_Mg22_simulated = UMAP_embedding(global_features_Mg22, 2, ax, 'red', 'Mg22 Simulated', 0.5, neighbors)
+    umap_2track_O16 = UMAP_embedding(global_features_2track, 2, ax, 'blue', 'O16 2 tracks', 0.5, neighbors)
+    umap_3track_O16 = UMAP_embedding(global_features_3track, 2, ax, 'green', 'O16 3 tracks', 0.5, neighbors)
     ax.legend()
-    plt.savefig('./clustering_plots/t_sne/2d_plots/plot2')
-    plt.close()   
+    plt.title("UMAP 2D Embedding")
 
-    # plot t-SNE clustering of experimental, 2Body, and 2Body_2T global features in 3D space
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    t_SNE_clustering(global_features_2Body, test_labels_2Body, '../simulated_data/voxel_data/output_digi_HDF_2Body/O16_size512', 3, ax, 'red', 'output_digi_HDF_2Body', 1)
-    t_SNE_clustering(global_features_2Body_2T, test_labels_2Body_2T, '../simulated_data/voxel_data/output_digi_HDF_2Body_2T/O16_size512', 3, ax, 'green', 'output_digi_HDF_2Body_2T', 1)
-    t_SNE_clustering(global_features_original, test_labels_original, data_file_stem, 3, ax, 'blue', 'experimental', 0.1)
-    ax.legend()
-    plt.savefig('./clustering_plots/t_sne/3d_plots/plot2')
-    plt.close()   
+    folder_path = "./plots/umap/2d_plots/umap_data"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    np.save(f'./plots/umap/2d_plots/umap_data/Mg22_Simulated_n{neighbors}.npy', umap_Mg22_simulated)
+    np.save(f'./plots/umap/2d_plots/umap_data/O16_2track_n{neighbors}.npy', umap_2track_O16)
+    np.save(f'./plots/umap/2d_plots/umap_data/O16_3track_n{neighbors}.npy', umap_3track_O16)
+    plt.savefig(f'./plots/umap/2d_plots/Mg22+O16_n_{neighbors}.png')
+    plt.close()  
+
+
+    # plot events of defined area of a cluster (change import files or comment if needed)
+    umap_Mg22 = np.load('./plots/umap/2d_plots/umap_data/Mg22_Simulated_n200.npy')
+    umap_3track = np.load('./plots/umap/2d_plots/umap_data/O16_3track_n200.npy')
+    umap_2track = np.load('./plots/umap/2d_plots/umap_data/O16_2track_n200.npy')
+    
+    plot_umap_events(umap_Mg22, 'Mg22_Simulated', '../simulated_data/process_data/Mg22/voxel_data/output_digi_HDF_Mg22_Ne20pp_8MeV/Mg22_size512')
+    plot_umap_events(umap_3track, '3_track', '../simulated_data/process_data/O16/voxel_data/output_digi_HDF_3Body/O16_size512')
+    plot_umap_events(umap_2track, '2_track', '../simulated_data/process_data/O16/voxel_data/output_digi_HDF_2Body/O16_size512')
 
 if __name__ == '__main__':
     extract_global_features()
