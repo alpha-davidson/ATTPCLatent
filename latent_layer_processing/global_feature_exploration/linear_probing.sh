61df5500a9111b8ece878e67942c8efc0cbc0c52
nialeksii <nialeksii@davidson.edu@alpha2.ada.davidson.edu>
2025-07-08 12:58:12 -0400

    changes to description

--- latent_layer_processing/global_feature_exploration/linear_probing.sh
+++ latent_layer_processing/global_feature_exploration/linear_probing.sh
@@ -4,4 +4,4 @@
 #SBATCH --gpus 1
 
 # Adapt the models sub-folder as needed to the correct file name. 
-PYTHONPATH=../../.. python3 -m ATTPCLatent.latent_layer_processing.global_feature_exploration.linear_probing --beam O16 --num-classes 24 ../../training/O16_models/2025-06-16-14:56:34/full_model ../../simulated_data/process_data/O16/voxel_data/output_digi_HDF_2Body/O16_size512 ../../simulated_data/process_data/O16/voxel_data/output_digi_HDF_3Body/O16_size512 
+PYTHONPATH=../../.. python3 -m ATTPCLatent.latent_layer_processing.global_feature_exploration.linear_probing --beam O16 --num-classes 24 path/to/model path/to/first_class_ds path/to/second_class_ds 
