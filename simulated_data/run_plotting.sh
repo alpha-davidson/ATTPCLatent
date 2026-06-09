dda478ef889dfb999d9630a0376c113f6912d5c3
Jogarenas <joarenas@davidson.edu>
2026-06-09 14:27:22 -0400

    Deleted the "Simulated Data" folder
    We did not need this folder anymorre as users will simply bring in their own numpy arrays instead of creating anything within the repo

--- simulated_data/run_plotting.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name "Plot_Exp_Data"
-#SBATCH --mem 32G
-#SBATCH --gpus 1
-
-# Calling this in the terminal will run the file 'plot_data.py.' Also, update the name of the file from which events will be plotted.
-python3 plot_data.py --beam Mg22 output_digi_HDF_Mg22_Ne20pp_8MeV
