665cf2348cc5836aa11c93e2f0142ee21c20260f
Jogarenas <joarenas@davidson.edu>
2026-06-11 16:10:11 -0400

    Additional Changes to the .sh file for the svm

--- latent_layer_processing/svm/run_svm.sh
+++ latent_layer_processing/svm/run_svm.sh
@@ -2,6 +2,8 @@
 #SBATCH --job-name "SVM_Training"
 #SBATCH --mem 32G
 #SBATCH --gpus 1
+#SBATCH --output=./results/svm_training_test1.out
+#SBATCH --error=./results/svm_training__test1.err
 
 # Adapt the models sub-folder as needed to the correct file name. 
 source $(conda info --base)/etc/profile.d/conda.sh
@@ -9,7 +11,7 @@ conda activate attpc-latent
 
 FEATURES_FILE="../../data/features.npy"
 LABELS_FILE="../../data/master_labels.npy"
-OUTPUT_DIR="./results/O16_svm_eval"
+OUTPUT_DIR="./results/svm_eval"
 SAMPLE_COUNT=250
 
 python3 svm.py "$FEATURES_FILE" "$LABELS_FILE" \
